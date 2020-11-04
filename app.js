53cd8d4186fdaf370b86373f7151b4e11abe4c8c
amrfatouh <66204477+amrfatouh@users.noreply.github.com>
2020-11-04 04:10:08 +0200

    creating server - routing - redirecting - creating file

---
+++ app.js
@@ -0,0 +1,35 @@
+const http = require("http");
+const fs = require("fs");
+
+const server = http.createServer(function (req, res) {
+  const url = req.url;
+  const method = req.method;
+
+  console.log(url);
+  if (url === "/") {
+    res.write("<h1>Welcome to Node.JS Guide!</h1>");
+    res.write(`
+      <form action='message' method='POST'>
+        <input autofocus type='text' name='message'/>
+        <input type='submit' value='Send' />
+      </form>
+    `);
+    res.end();
+  } else if (url === "/message") {
+    const body = [];
+    req.on("data", (chunk) => {
+      body.push(chunk);
+    });
+    req.on("end", () => {
+      const parsedMessage = Buffer.concat(body).toString();
+      const message = parsedMessage.split("=")[1];
+      fs.writeFile("./message.txt", message, () => {
+        res.statusCode = 302;
+        res.setHeader("Location", "/");
+        res.end();
+      });
+    });
+  }
+});
+
+server.listen(8000);
